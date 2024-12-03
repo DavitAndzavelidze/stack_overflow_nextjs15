78b1ca3f2fb8e69d37ae80bf11e4f89e3b31b284
Davit Andzavelidze <davit.andzavelidze@gmail.com>
2024-12-03 19:02:01 +0400

    add light and dark theme & shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-orange-500">
-    Hello to next js world
-  </h1>
+  <>
+    <h1>Hello to next js world</h1>
+  </>
 );
 export default Home;
